8c05166b9b15d64b782b148f3157c5b9ce448954
YishaiSeela <yishai320ds@gmail.com>
2017-04-02 21:57:32 +0300

    project upload

---
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/Searcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib
+{
+    public abstract class Searcher : ISearcher
+    {
+        private MyPriorityQueue<State> openList;
+        private int evaluatedNodes;
+        public Searcher()
+        {
+            openList = new MyPriorityQueue<State>();
+            evaluatedNodes = 0;
+        }
+        protected State popOpenList()
+        {
+            evaluatedNodes++;
+            return openList.poll();
+        }
+        // a property of openList
+        public int OpenListSize
+        { // it is a read-only property :)
+            get { return openList.Count; }
+        }
+        // ISearcher’s methods:
+        public int getNumberOfNodesEvaluated()
+        {
+            return evaluatedNodes;
+        }
+        public abstract Solution search(ISearchable searchable);
+    }
+}
